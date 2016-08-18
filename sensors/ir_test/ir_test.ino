25d4fc1dea1e8a341a198f538283868a7d67da6b
Pierre Karashchuk <krchtchk@gmail.com>
2016-08-18 02:28:13 -0700

    add sensors code

---
+++ sensors/ir_test/ir_test.ino
@@ -0,0 +1,31 @@
+
+
+/* ReadSharpIR.pde
+*  simple sketch to read values from a IR Distance Sensor
+*/ 
+#define N_SENSORS 3
+
+int value;
+int sensors[] = {A0, A1, A2};
+int i;
+
+void setup()
+{
+    Serial.begin(115200);            // setup serial port to laptop to print values
+}
+
+void loop() 
+{
+  for(i=0; i<N_SENSORS; i++) {
+    value = analogRead(sensors[i]);
+    Serial.print(value);
+    if(i != N_SENSORS - 1) {
+      Serial.print(", ");
+    } else {
+      Serial.println();
+    }
+  }
+
+    delay(100);                                               // short delay 
+}
+
